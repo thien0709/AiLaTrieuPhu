6da1d8d1da566dd39f0bb453610112e99506fa56
thien0709 <thien070904@gmail.com>
2023-04-30 14:47:42 +0700

    update box question answer

--- access/main.js
+++ access/main.js
@@ -1,5 +1,27 @@
-// Get button
-const btn = document.querySelector("#box button");
+// Get button submit
+const btn1 = document.querySelector("#box .btn1");
 // Get question
-const box = document.querySelector("#box p");
-console.log(box.textContent);
+const qs = document.querySelector("#box .p");
+// Get answer
+const ans = document.querySelectorAll("#box .answer-box input");
+console.log(btn1, btn2, qs, ans);
+// Record question
+let count = 1;
+let questions = [];
+btn1.addEventListener("click", function () {
+  let question = {
+    stt: count++,
+    qs: qs.value,
+    ans1: ans[0].value,
+    ans2: ans[1].value,
+    ans3: ans[2].value,
+    ans4: ans[3].value,
+  };
+  questions.push(question);
+  localStorage.setItem("questions", JSON.stringify(questions));
+});
+
+
+// Get button start
+const btn2 = document.querySelector("#box .btn2");
+//
