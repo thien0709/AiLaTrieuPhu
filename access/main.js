92e3f6b56cde6831982755b31affeb1493ef08e3
thien0709 <thien070904@gmail.com>
2023-05-06 23:59:03 +0700

    update

--- access/main.js
+++ access/main.js
@@ -5,15 +5,21 @@ let count = 1;
 let cqs = 2;
 let questions = [];
 btn1.addEventListener("click", function () {
-  const boxqs = document.querySelector("#box #boxqs"); // Get new box 
+  const boxqs = document.querySelector("#box #boxqs"); // Get new box
   const qs = document.querySelector("#box #boxqs .p"); // Get question
-  const ans = document.querySelectorAll("#box #boxqs .answer-box input"); // Get answer  
+  const ans = document.querySelectorAll("#box #boxqs .answer-box input"); // Get answer
   // Check value
-  if(qs.value.length == 0 || ans[0].value.length == 0 ||ans[1].value.length == 0 || ans[2].value.length == 0 ||ans[3].value.length == 0 ){
+  if (
+    qs.value.length == 0 ||
+    ans[0].value.length == 0 ||
+    ans[1].value.length == 0 ||
+    ans[2].value.length == 0 ||
+    ans[3].value.length == 0
+  ) {
     qs.focus();
     alert("Hay nhap dung format! ");
     return;
-  } 
+  }
   // Create new box
   boxqs.innerHTML = `<h2>Nhập câu hỏi ${cqs++}</h2>
   <input type="text" placeholder="This is a question" class="p">
@@ -40,13 +46,14 @@ const btn2 = document.querySelector("#box .btn2");
 const box = document.querySelector("#box");
 btn2.addEventListener("click", function () {
   box.style.display = "none";
-  const questionContent = document.querySelector("#content #question");// Get question
-  const answerContent = document.querySelector("#content #answer");// Get answer
-  ;
+  const questionContent = document.querySelector("#content #question"); // Get question
+  const answerContent = document.querySelector("#content #answer"); // Get answer
   // Get question in localStorage
   let localQuestion = JSON.parse(localStorage.getItem("questions"));
-  for(let i = 0 ;i <= questions.length; i++){
-    questionContent.innerText = localQuestion[i].qs;
-    
+  // Render question
+  for (let i = 0; i <= questions.length; i++) {
+    setTimeout(() => {
+          questionContent.innerText = localQuestion[i].qs;
+    }, 15000);
   }
 });
